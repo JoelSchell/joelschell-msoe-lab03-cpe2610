e3b9fc71ad2006ee7db0a36fb5f8c418d5b88472
Joel Schellinger <schellingerjf@msoe.edu>
2026-02-03 10:18:59 -0600

    added delay library

---
+++ Src/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ *  Created on: Jan 25, 2026
+ *      Author: schellingerjf
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include <stdint.h>
+
+void delay_ms(uint32_t ms);
+void delay_ms_pause_check(uint32_t ms);
+
+#endif /* DELAY_H_ */
